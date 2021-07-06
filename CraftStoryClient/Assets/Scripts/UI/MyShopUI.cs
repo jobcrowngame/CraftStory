9262f0c0ec7d13c92f581b5b396415eb84c2871f
jobcrowngame1 <jobcrown.game.1@gmail.com>
2021-07-06 19:06:26 +0900

    update

--- CraftStoryClient/Assets/Scripts/UI/MyShopUI.cs
+++ CraftStoryClient/Assets/Scripts/UI/MyShopUI.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+ï»¿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,7 +14,7 @@ public class MyShopUI : UIBase
         MyShopLG.E.Init(this);
 
         title = FindChiled<TitleUI>("Title");
-        title.SetTitle("ƒ}ƒCƒVƒ‡ƒbƒv");
+        title.SetTitle("ãƒžã‚¤ã‚·ãƒ§ãƒƒãƒ—");
         title.SetOnClose(() => { Close(); });
         title.EnActiveCoin(2);
         title.EnActiveCoin(3);
