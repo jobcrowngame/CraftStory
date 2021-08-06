0af454c076b139ddc0a989962c480cbd47546f74
jobcrowngame1 <jobcrown.game.1@gmail.com>
2021-08-06 09:37:42 +0900

    IOS ビルド対応

--- CraftStoryClient/Assets/Editor/PostXcodeBuild.cs
+++ CraftStoryClient/Assets/Editor/PostXcodeBuild.cs
@@ -1,8 +1,6 @@
 using System.IO;
 using UnityEditor;
 using UnityEditor.Callbacks;
-using System.Collections;
-using System.Collections.Generic;
 #if UNITY_IOS
 using UnityEditor.iOS.Xcode;
 #endif
@@ -10,20 +8,55 @@ using UnityEditor.iOS.Xcode;
 public class PostXcodeBuild
 {
 #if UNITY_IOS
-        [PostProcessBuild]
-        public static void SetXcodePlist(BuildTarget buildTarget, string pathToBuiltProject)
+    [PostProcessBuild]
+    public static void SetXcodePlist(BuildTarget buildTarget, string pathToBuiltProject)
+    {
+        if (buildTarget != BuildTarget.iOS)
         {
-            if (buildTarget != BuildTarget.iOS) return;
+            return;
+        }
 
-            var plistPath = Path.Combine(pathToBuiltProject , "Info.plist");
+        // pbx
+        {
+            var projectPath = PBXProject.GetPBXProjectPath(pathToBuiltProject);
+            var pbx = new PBXProject();
+            pbx.ReadFromFile(projectPath);
+            string targetGuid = GetTargetGuid(pbx);
+
+            // admob
+            pbx.AddFrameworkToProject(targetGuid, "AppTrackingTransparency.framework", true);
+
+            pbx.WriteToFile(projectPath);
+        }
+
+        // plist
+        {
+            var plistPath = Path.Combine(pathToBuiltProject, "Info.plist");
             var plist = new PlistDocument();
             plist.ReadFromFile(plistPath);
 
-            // set info.plist
+            // skad
             var array = plist.root.CreateArray("SKAdNetworkItems");
             PlistElementDict dict = array.AddDict();
             dict.SetString("SKAdNetworkIdentifier", "cstr6suwn9.skadnetwork");
             File.WriteAllText(plistPath, plist.WriteToString());
+
+            var root = plist.root;
+
+            // track description
+            root.SetString("NSUserTrackingUsageDescription", "本アプリは広告効果測定・分析のためにIDFA(広告識別子)を利用します。");
+            plist.WriteToFile(plistPath);
         }
+
+    }
+
+    private static string GetTargetGuid(PBXProject pbx)
+    {
+#if UNITY_2019_3_OR_NEWER
+        return pbx.GetUnityFrameworkTargetGuid();
+#else
+        return pbx.TargetGuidByName(PBXProject.GetUnityTargetName());
+#endif
+    }
 #endif
 }
---
+++ CraftStoryClient/Assets/Scripts/Cor/AttObjC.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using UnityEngine;
+
+public class AttObjC : MonoBehaviour
+{
+    void Start()
+    {
+#if UNITY_IOS
+        int status = AttObjC.GetTrackingAuthorizationStatus();
+        if (status == 0)
+        {
+            AttObjC.RequestTrackingAuthorization();
+        }
+        Debug.Log("ATT状況：" + status);
+#endif
+    }
+
+#if UNITY_IOS
+    private const string DLL_NAME = "__Internal";
+    [DllImport(DLL_NAME)]
+    private static extern int Sge_Att_getTrackingAuthorizationStatus();
+    private static int GetTrackingAuthorizationStatus()
+    {
+        if (Application.isEditor)
+        {
+            return -1;
+        }
+        return Sge_Att_getTrackingAuthorizationStatus();
+    }
+    [DllImport(DLL_NAME)]
+    private static extern void Sge_Att_requestTrackingAuthorization(OnCompleteCallback callback);
+
+    private delegate void OnCompleteCallback(int status);
+    private static SynchronizationContext _context;
+    private static Action _onComplete;
+
+    private static void RequestTrackingAuthorization()
+    {
+        if (Application.isEditor)
+        {
+            return;
+        }
+#if UNITY_IOS
+        _context = SynchronizationContext.Current;
+        Sge_Att_requestTrackingAuthorization(OnRequestComplete);
+#endif
+    }
+    [AOT.MonoPInvokeCallback(typeof(OnCompleteCallback))]
+    private static void OnRequestComplete(int status)
+    {
+        if (_onComplete != null)
+        {
+            _context.Post(_ => {
+                _onComplete = null;
+            }, null);
+        }
+    }
+#endif
+}
