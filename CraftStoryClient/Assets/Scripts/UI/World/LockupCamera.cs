8963dc3a0d6766a874ed168ce0fa7919e4c1da12
jobcrowngame1 <jobcrown.game.1@gmail.com>
2021-10-27 14:38:21 +0900

    NPCに吹き出しを追加

---
+++ CraftStoryClient/Assets/Scripts/UI/World/LockupCamera.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+
+public class LockupCamera : MonoBehaviour
+{
+    private void Update()
+    {
+        //　カメラと同じ向きに設定
+        transform.rotation = Camera.main.transform.rotation;
+    }
+}
