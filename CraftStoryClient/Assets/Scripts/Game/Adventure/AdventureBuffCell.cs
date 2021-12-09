678328c50ee05b8da0f7fda0fe7dc882e600ac73
jobcrowngame1 <jobcrown.game.1@gmail.com>
2021-12-09 09:20:20 +0900

    v2.0.8 冒険エリアBUFF機能

--- CraftStoryClient/Assets/Scripts/Game/Adventure/AdventureBuffCell.cs
+++ CraftStoryClient/Assets/Scripts/Game/Adventure/AdventureBuffCell.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class AdventureBuffCell : CharacterBase
 {
     AdventureBuff config;
+    Vector3 targetPos;
+    bool isActived;
 
     private void OnTriggerEnter(Collider other)
     {
+        if (!isActived)
+            return;
+
         if ((CharacterGroup)config.TargetGroup == CharacterGroup.Player)
         {
             var character = other.GetComponent<CharacterBase>();
@@ -20,9 +25,29 @@ public class AdventureBuffCell : CharacterBase
         }
     }
 
+    private void FixedUpdate()
+    {
+        if (!isActived)
+        {
+            if (Vector3.Distance(transform.position, targetPos) > 0.1f)
+            {
+                transform.position += Vector3.down * 0.03f;
+            }
+            else
+            {
+                isActived = true;
+            }
+        }
+    }
+
     public void Set(AdventureBuff config)
     {
         this.config = config;
+
+        targetPos = transform.position;
+        transform.position = new Vector3(transform.position.x, transform.position.y + 10, transform.position.z);
+
+        isActived = false;
     }
 
     private void UserSkill()
