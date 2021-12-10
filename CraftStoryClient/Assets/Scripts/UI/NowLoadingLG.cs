ada9291dd0ca8b5f83423b91ed3fd3e417e0dae8
jobcrowngame1 <jobcrown.game.1@gmail.com>
2021-12-10 11:52:39 +0900

    ローディング機能変更

--- CraftStoryClient/Assets/Scripts/UI/NowLoadingLG.cs
+++ CraftStoryClient/Assets/Scripts/UI/NowLoadingLG.cs
@@ -16,12 +16,25 @@ public class NowLoadingLG : UILogicBase<NowLoadingLG, NowLoadingUI>
     {
         // シーンの読み込みをする
         async = SceneManager.LoadSceneAsync(NextSceneName);
+        async.allowSceneActivation = false;
+
+        float curPercent = 0;
 
         //　読み込みが終わるまで進捗状況をスライダーの値に反映させる
         while (!async.isDone)
         {
-            var progressVal = Mathf.Clamp01(async.progress / 0.9f);
+            curPercent = async.progress > UI.Percent ? UI.Percent : async.progress;
+
+            Debug.LogWarning(curPercent);
+
+            var progressVal = Mathf.Clamp01(curPercent / 0.9f);
             UI.SetSlider(progressVal);
+
+            if (curPercent >= 0.9f)
+            {
+                async.allowSceneActivation = true;
+            }
+
             yield return null;
         }
     }
--- CraftStoryClient/Assets/Scripts/UI/NowLoadingUI.cs
+++ CraftStoryClient/Assets/Scripts/UI/NowLoadingUI.cs
@@ -5,15 +5,25 @@ public class NowLoadingUI : UIBase
     Text text { get => FindChiled<Text>("Text"); }
     Slider slider { get => FindChiled<Slider>("Slider"); }
 
+    float timer = 0;
+    public float Percent { get => timer / 3; }
+
     private void Start()
     {
         base.Init();
         NowLoadingLG.E.Init(this);
 
+        timer = 0;
+
         text.text = "Now Loading...";
         slider.value = 0;
 
         StartCoroutine(NowLoadingLG.E.LoadData());
+    }
+
+    private void FixedUpdate()
+    {
+        timer += 0.02f;
     }
 
     public void SetSlider(float v)
