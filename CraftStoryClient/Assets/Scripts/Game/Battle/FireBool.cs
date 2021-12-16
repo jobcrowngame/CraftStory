9735b11ba6216fe4d707a09c9d81bbc959927472
jobcrowngame1 <jobcrown.game.1@gmail.com>
2021-12-16 10:21:28 +0900

    エラー修正

---
+++ CraftStoryClient/Assets/Scripts/Game/Battle/FireBool.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class FireBool : MonoBehaviour
+{
+    CharacterBase attacker;
+    CharacterBase target;
+    float speed;
+
+    public void Update()
+    {
+        if (target == null)
+            return;
+        
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, Time.deltaTime * speed);
+        transform.LookAt(target.transform);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var entity = other.GetComponent<CharacterBase>();
+        if (entity == null)
+        {
+            entity.AddImpact(entity, attacker, 1);
+        }
+
+        EffectMng.E.AddBattleEffect("FireBollBlast01", 3, target.transform);
+
+        Destroy(gameObject);
+    }
+
+    public void SetInfo(CharacterBase attacker, CharacterBase target, float speed)
+    {
+        this.attacker = attacker;
+        this.target = target;
+        this.speed = speed;
+    }
+}
